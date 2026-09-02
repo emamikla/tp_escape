289e09ba512b5aaa0c4494e04b986b534334c19e
JULIETA ROMANO <49703405@ort>
2026-09-02 08:58:17 -0300

    fin clase 1

--- Controllers/EscapeController.cs
+++ Controllers/EscapeController.cs
@@ -6,8 +6,27 @@ namespace tp_escape.Controllers;
 
 public class EscapeController : Controller
 {
-        public IActionResult Index()
+    public BD bd = new BD();
+    public IActionResult Index()
     {
         return View();
     }
+
+    public IActionResult FijarseNombreDeUsuario (string nombreUsuario)
+    {
+        if (bd.ExisteUsuario(nombreUsuario) == true)
+        {
+            int salaActual = bd.ObtenerSalaActual(nombreUsuario);
+            return RedirectToAction("Sala" + salaActual, new { nombreUsuario = nombreUsuario });
+        }
+        else
+        {
+            bd.CrearPartida(nombreUsuario, 1);
+            return RedirectToAction("Sala1", new { nombreUsuario = nombreUsuario });
+        }
+
+    }
+
+    
 }
+
