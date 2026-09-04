f8b8350da5ccfa1387acf1d92eee9fdee905df1a
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-09-04 09:03:10 -0300

    va queriendo la cosa

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function mostrarPista() {
+    const pista = document.getElementById('pista');
+    const boton = document.getElementById('mostrarPista');
+
+    if (pista && boton) {
+        if (pista.style.display === 'none') {
+            pista.style.display = 'block';
+            boton.textContent = 'Ocultar pista';
+        } else {
+            pista.style.display = 'none';
+            boton.textContent = 'Mostrar pista';
+        }
+    }
+}
